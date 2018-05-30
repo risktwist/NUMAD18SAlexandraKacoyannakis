4fd761112058b5825b75aab9a3a8e887d9f4b5f0
lkacoy <lkacoyannakis@gmail.com>
2018-05-29 20:10:29 -0400

    Implement clear button

--- app/src/main/java/alexandrakacoyannakis/madcourse/neu/edu/numad18s_alexandrakacoyannakis/Dictionary.java
+++ app/src/main/java/alexandrakacoyannakis/madcourse/neu/edu/numad18s_alexandrakacoyannakis/Dictionary.java
@@ -1,7 +1,12 @@
 package alexandrakacoyannakis.madcourse.neu.edu.numad18s_alexandrakacoyannakis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Dictionary extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class Dictionary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        //clear button
+        final Button clearButton = findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText inputText = findViewById(R.id.enter_word);
+                TextView resultsView = findViewById(R.id.entries);
+                inputText.setText("");
+                resultsView.setText("");
+            }
+        });
+
+        //acknowledgements button
+        final Button acknowledgementsButton = findViewById(R.id.acknowledgements_button);
+        acknowledgementsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
