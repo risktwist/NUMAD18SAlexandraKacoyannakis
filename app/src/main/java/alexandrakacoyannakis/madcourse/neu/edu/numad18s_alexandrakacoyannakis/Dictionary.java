6d3a0c2823c9432acd8313ee9baa45de27c6c3fc
lkacoy <lkacoyannakis@gmail.com>
2018-05-29 22:51:19 -0400

    read from word list file and add to an ArrayList

--- app/src/main/java/alexandrakacoyannakis/madcourse/neu/edu/numad18s_alexandrakacoyannakis/Dictionary.java
+++ app/src/main/java/alexandrakacoyannakis/madcourse/neu/edu/numad18s_alexandrakacoyannakis/Dictionary.java
@@ -1,21 +1,50 @@
 package alexandrakacoyannakis.madcourse.neu.edu.numad18s_alexandrakacoyannakis;
 
-import android.content.Intent;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Dictionary extends AppCompatActivity {
+
+    private MediaPlayer mMediaPlayer;
+    InputStream inputStream = null;
+    BufferedReader reader = null;
+    ArrayList<String> matches = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionary);
+
+        mMediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI );
+        try {
+            inputStream = getAssets().open("wordlist.txt");
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                matches.add(line);
+            }
+            reader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            Log.e("message: ",e.getMessage());
+        }
+
 
         //edit text
         final EditText inputText = findViewById(R.id.enter_word);
@@ -27,7 +56,7 @@ public class Dictionary extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                
+                mMediaPlayer.start();
             }
 
             @Override
