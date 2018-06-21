f682353fe896962f79e9610c77e4145054d3e8b8
lkacoy <lkacoyannakis@gmail.com>
2018-06-20 21:59:32 -0400

    add countdown timer

--- app/src/main/java/alexandrakacoyannakis/madcourse/neu/edu/numad18s_alexandrakacoyannakis/ControlFragment.java
+++ app/src/main/java/alexandrakacoyannakis/madcourse/neu/edu/numad18s_alexandrakacoyannakis/ControlFragment.java
@@ -2,9 +2,13 @@ package alexandrakacoyannakis.madcourse.neu.edu.numad18s_alexandrakacoyannakis;
 
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class ControlFragment extends Fragment {
@@ -28,6 +32,25 @@ public class ControlFragment extends Fragment {
                 ((GameActivity) getActivity()).restartGame();
             }
         });
+
+        //create timer with default 3 minutes
+        final TextView timerView = rootView.findViewById(R.id.timer);
+        new CountDownTimer(180000, 1000) {
+            public void onTick(long timeToFinished) {
+                timerView.setText(""+String.format("%02d:%02d:%02d",
+                        TimeUnit.MILLISECONDS.toHours(timeToFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(timeToFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(timeToFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(timeToFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(timeToFinished))));
+            }
+
+            public void onFinish() {
+                timerView.setText("Time's Up!");
+            }
+        }.start();
+
+
         return rootView;
     }
 }
